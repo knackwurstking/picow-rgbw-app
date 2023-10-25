726f319e66830b013994bcb50d5a3208b86447de
Udo Bauer <knackwurstking.tux@gmail.com>
2023-10-25 17:36:06 +0200

    +dialog basic stuff

--- src/lib/components/dialogs/DeviceSettings.svelte
+++ src/lib/components/dialogs/DeviceSettings.svelte
@@ -1,8 +1,50 @@
 <script>
-    import { Dialog } from "svelte-css";
+    import { Button, Dialog, DialogFooter, DialogHeader } from "svelte-css";
+
+    /***********
+     * Bindings
+     ***********/
 
     /** @type {Dialog} */
     let dialog;
+
+    /******************************
+     * Function Export Definitions
+     ******************************/
+
+    export async function open() {
+        dialog.showModal();
+    }
+
+    export async function close() {
+        dialog.close();
+    }
+
+    /***********************
+     * Function Definitions
+     ***********************/
+
+    async function clickSubmit() {
+        // TODO: write device settings to store and close the dialog
+        close();
+    }
 </script>
 
-<Dialog bind:this={dialog} />
+<Dialog bind:this={dialog}>
+    <DialogHeader
+        title="Settings"
+        on:close={() => close()}
+    />
+
+    <section>
+        <!-- TODO: device name change (input) -->
+    </section>
+
+    <DialogFooter>
+        <Button
+            color="primary"
+            variant="full"
+            on:click={() => clickSubmit()}
+        >OK</Button>
+    </DialogFooter>
+</Dialog>
