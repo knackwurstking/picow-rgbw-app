9628f854a5faae95ac670f4c987c8cd480239d53
Udo Bauer <knackwurstking.tux@gmail.com>
2023-10-25 18:05:44 +0200

    +todo +device settings icon import + main.container and height

--- src/Main.svelte
+++ src/Main.svelte
@@ -1,14 +1,19 @@
 <script>
-    import SettingsIcon from "svelte-material-icons/CogOutline.svelte";
+    import DeviceSettingsIcon from "svelte-material-icons/DotsVertical.svelte";
     import AddIcon from "svelte-material-icons/PlusCircleOutline.svelte";
     import TrashIcon from "svelte-material-icons/TrashCanOutline.svelte";
 
     import { Components } from "./lib";
 </script>
 
-<main />
+<main class="container">
+    <!-- TODO: 1. devices list, 2. color storage, 3. color picker -->
+</main>
 
 <Components.DeviceSettingsDialog />
 
 <style>
+    main {
+        height: calc(100% - 4em);
+    }
 </style>
