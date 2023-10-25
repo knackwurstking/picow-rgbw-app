800cb9d67dcfc161a891d69166fb45aae8102d28
Udo Bauer <knackwurstking.tux@gmail.com>
2023-10-25 17:35:32 +0200

    +states files and exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,2 @@
-export * from "./components/dialogs";
+export * as Components from "./components/dialogs";
+export * as States from "./states";
---
+++ src/lib/states/index.js
@@ -0,0 +1,3 @@
+export { default as api } from "./api";
+export { default as devices } from "./devices";
+export { default as theme } from "./theme";
