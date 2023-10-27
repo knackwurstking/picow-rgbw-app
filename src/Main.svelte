2583129193700cf02475ffdad2d02e0e2567459a
Udo Bauer <knackwurstking.tux@gmail.com>
2023-10-28 00:09:36 +0200

    Render a list with devices (@todo: device-settings button and color preview code tag)

--- src/Main.svelte
+++ src/Main.svelte
@@ -12,9 +12,16 @@
      * @type {import("./lib/api").Device}
      */
 
-    /**
+    /***********************
+     * Variable Definitions
+     ***********************/
+
+    /** @type {Device[]} */
+    let selected = [];
+
+    /*****************
      * Store: devices
-     */
+     *****************/
 
     let devices = States.devices.create((devices) => {
         Api.getDevices()
@@ -44,17 +51,51 @@
             <h2>Devices</h2>
         </section>
 
-        <section class="is-debug">
+        <section class="has-padding">
             <!-- TODO: render the devices list here ... -->
+
             <ul class="devices-list">
                 {#each $devices as device}
-                    <li class="device">
+                    <li
+                        class="device"
+                        class:checked={
+                            !!selected.find(
+                                d => (d.host === device.host && d.port === device.port)
+                            )
+                        }
+                    >
+                        <div class="background" />
                         <Label
+                            class="has-padding"
                             primaryText={device.name}
                             secondaryText={`${device.host}:${device.port}`}
                             useLabel
                         >
-                            <input type="checkbox" style="display: none;"/>
+                            <input
+                                type="checkbox"
+                                style="display: none;"
+                                on:change={(ev) => {
+                                    const i = selected.findIndex(
+                                        (d) => (d.host === device.host && d.port === device.port)
+                                    );
+
+                                    if (i > -1) {
+                                        if (ev.currentTarget.checked) return;
+
+                                        selected = [
+                                            ...selected.slice(0, i),
+                                            ...selected.slice(i+1),
+                                        ];
+                                    } else {
+                                        if (ev.currentTarget.checked) {
+                                            selected = [
+                                                ...selected,
+                                                device,
+                                            ];
+                                        }
+                                    }
+                                }}
+                            />
                         </Label>
                     </li>
                 {/each}
@@ -68,5 +109,30 @@
 <style>
     main {
         height: calc(100% - 3.5em);
+    }
+
+    .devices .devices-list .device .background {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+
+        background-color: currentColor;
+        opacity: 0;
+
+        transition: opacity .25s linear;
+    }
+
+    .devices .devices-list .device.checked .background {
+        opacity: 0.1;
+    }
+
+    .devices .devices-list .device {
+        border-bottom: .1em solid hsl(var(--border));
+    }
+
+    .devices .devices-list .device:first-child {
+        border-top: .1em solid hsl(var(--border));
     }
 </style>
