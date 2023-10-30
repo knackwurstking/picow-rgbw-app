9a7c24fcfdc79fba4fcf618ca75d9bbc0ead455b
Udo Bauer <knackwurstking.tux@gmail.com>
2023-10-30 01:00:25 +0100

    Add color storage

---
+++ src/lib/states/color/index.js
@@ -0,0 +1,29 @@
+import { writable } from "svelte/store";
+
+/**
+ * @typedef Color
+ * @type {import("../../api").ApiColor}
+ *
+ * @typedef StateColor
+ * @type {import("svelte/store").Writable<Color>}
+ */
+
+const storageKey = "color";
+
+/** @type {StateColor} */
+const color = writable((() => {
+    return JSON.parse(
+        localStorage.getItem(storageKey) ||
+            '{ "r": 100, "g": 100, "b": 100 }'
+    );
+})());
+
+color.subscribe((cs) => {
+    localStorage.setItem(storageKey, JSON.stringify(cs));
+});
+
+export function create() {
+    return {
+        ...color,
+    };
+}
--- src/lib/states/index.js
+++ src/lib/states/index.js
@@ -2,3 +2,4 @@ export * as devices from "./devices";
 export * as theme from "./theme";
 export * as server from "./server";
 export * as colorStorage from "./color-storage";
+export * as color from "./color";
